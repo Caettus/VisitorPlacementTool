d658093ebd2ef45f69423618f48a2d9c19291dea
Caettus <vincentscheffer2@gmail.com>
2023-06-05 14:30:20 +0200

    unversioned files

---
+++ VPTTest/TournamentTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace VPTTest;
+
+[TestClass]
+public class TournamentTest
+{
+    [TestMethod]
+    public void CreateSectors_Test()
+    {
+        // Arrange
+        // Act
+        
+        // Assert
+        
+    }
+}
